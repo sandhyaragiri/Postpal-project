b66768bcc6d21e7777558c56fce32a0e787bd0a5
Sandhya <ragirisandhya24@gmail.com>
2026-04-07 20:27:07 +0530

    created chat

--- Script.js
+++ Script.js
@@ -1,7 +1,27 @@
 function checkText(){
-    let text = document.getElementById("message").value;
+    let text = document.getElementById("message").value.toLowerCase();
     if(text.trim() === ""){
         document.getElementById("text").innerText = "Bro write something first 😅";
+    }
+
+    else if(text.trim() === "hi"){
+        document.getElementById("text").innerText = "Hi! How you doing bro";
+    }
+
+    else if(text.trim() === "how are you"){
+        document.getElementById("text").innerText = "I'm great bro";
+    }
+
+    else if(text.trim() === "bye"){
+        document.getElementById("text").innerText = "bye! see you around";
+    }
+
+    else if(text.trim() === "hello"){
+        document.getElementById("text").innerText = "Hello! Bro😃 wassup";
+    }
+
+    else if(text.trim() === "nothing"){
+        document.getElementById("text").innerText = "Then type something dudu!😃";
     }
 
     else {
